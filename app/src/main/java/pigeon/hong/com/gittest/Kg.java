0875e50a16b4d4f0f5602899f65b3c21b39adb95
GitTest <583406381@qq.com>
2016-05-04 12:34:07 +0800

    新添加

--- app/src/main/java/pigeon/hong/com/gittest/Kg.java
+++ app/src/main/java/pigeon/hong/com/gittest/Kg.java
@@ -4,4 +4,8 @@ package pigeon.hong.com.gittest;
  * Created by honger on 16/4/29.
  */
 public class Kg {
+
+
+
+    //－－－－－－－－－
 }
---
+++ app/src/main/java/pigeon/hong/com/gittest/Ui.java
@@ -0,0 +1,7 @@
+package pigeon.hong.com.gittest;
+
+/**
+ * Created by honger on 16/5/4.
+ */
+public class Ui {
+}
